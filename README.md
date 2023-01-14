dda53aebc89128920d1ce4d2a5e8141c2e441804
Cleverton Santos <clevertonk23@gmail.com>
2023-01-14 19:29:24 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-canil
+Projeto feit em Node + Typescript
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rdar o projeto
+`npm run start-dev`
---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as PageController from '../controllers/pagecontroller';
+import * as searchController from '../controllers/searchControllers';
+
+
+const router = Router();
+
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('search', searchController.search);
+
+export default router;
